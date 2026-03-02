74e3131c436986b9a7b4b560b345b60448ce8416
tlhale <tlhalesebaeng@gmail.com>
2026-03-02 17:10:39 +0200

    Final touch ups before showing the spinner

--- script.js
+++ script.js
@@ -11,39 +11,51 @@ const searchCountry = async (countryName) => {
         spinner = document.getElementById('loading-spinner');
         spinner.style.display = 'block';
 
+        document.getElementById('error-message').style.display = 'none';
+        document.getElementById('country-info').style.display = 'none';
+
         // Fetch country data
         const country = await getCountry(countryName, 'name');
 
-        // Update DOM
-        document.getElementById('country-info').innerHTML = `
-            <section>
-                <h2>${country.name.common}</h2>
-                <p><strong>Capital:</strong> ${country.capital[0]}</p>
-                <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-                <p><strong>Region:</strong> ${country.region}</p>
-            </section>
-            <img src="${country.flags.svg}" alt="${country.name.common} flag">
-        `;
+        if(country) {
+            document.getElementById('country-info').style.display = 'flex';
 
-        country.borders.map(async (borderId) => {
-            // Fetch bordering countries
-
-            const border = await getCountry(borderId, 'alpha');
-
-            // Update bordering countries section
-
-            const html = `
+            // Update DOM
+            document.getElementById('country-info').innerHTML = `
                 <section>
-                    <h2>${border.name.common}</h2>
-                    <img src="${border.flags.svg}" alt="${border.name.common} flag">
+                    <h2>${country.name.common}</h2>
+                    <p><strong>Capital:</strong> ${country.capital[0]}</p>
+                    <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+                    <p><strong>Region:</strong> ${country.region}</p>
                 </section>
+                <img src="${country.flags.svg}" alt="${country.name.common} flag">
             `;
 
-            document.getElementById('bordering-countries').innerHTML += html;
-        });
+            document.getElementById('bordering-countries').style.display = 'grid';
 
+            country.borders.map(async (borderId) => {
+                // Fetch bordering countries
+
+                const border = await getCountry(borderId, 'alpha');
+
+                // Update bordering countries section
+
+                const html = `
+                    <section>
+                        <h2>${border.name.common}</h2>
+                        <img src="${border.flags.svg}" alt="${border.name.common} flag">
+                    </section>
+                `;
+
+                document.getElementById('bordering-countries').innerHTML += html;
+            });
+        } else {
+            throw new Error('Country not found. Please check your name and try again')
+        }
     } catch(error) {
-
+        const errorSection = document.getElementById('error-message');
+        errorSection.style.display = 'block';
+        errorSection.innerHTML = `<p>${error.message || "An error occurred please try again later"}</p>`
     } finally {
         spinner.style.display = 'none';
     }
