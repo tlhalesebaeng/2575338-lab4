6259eef1ea94587787afe816c30590921a37a998
tlhale <tlhalesebaeng@gmail.com>
2026-03-02 15:17:51 +0200

    Create a helper function to get the country data

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const getCountryInfo = async () => {
+const getCountry = async (countryName) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+    const data = await response.json();
+    return data[0];
+}
+
+const getCountryInfo = async (countryName) => {
     let spinner = null;
     try {
         // Show loading spinner
         spinner = document.getElementById('loading-spinner');
         spinner.style.display = 'block';
+
+        // Fetch country data
+        const country = await getCountry(countryName);
+        console.log(country);
     } catch(error) {
 
     } finally {
